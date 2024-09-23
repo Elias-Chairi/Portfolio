5a2d4b2f58ce587e6e0f60fc45af3aa2ce9b1ba8
Elias <elias@dhcp-10-22-126-32.wlan.ntnu.no>
2024-09-23 13:02:51 +0200

    fix: using env variable for studio base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,13 @@ const {
   PUBLIC_SANITY_STUDIO_PROJECT_ID,
   PUBLIC_SANITY_STUDIO_DATASET,
   PUBLIC_SANITY_PROJECT_ID,
-  PUBLIC_SANITY_DATASET
+  PUBLIC_SANITY_DATASET,
+  PUBLIC_SANITY_STUDIO_BASE_PATH
 } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
 const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
+const studioBasePath = PUBLIC_SANITY_STUDIO_BASE_PATH || "/admin";
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,7 +24,7 @@ export default defineConfig({
     sanity({
       projectId,
       dataset,
-      studioBasePath: "/admin",
+      studioBasePath,
       useCdn: false,
       // `false` if you want to ensure fresh data
       apiVersion: "2023-03-20" // Set to date of setup to use the latest API version
